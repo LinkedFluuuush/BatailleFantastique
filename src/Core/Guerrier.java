5d68ab609e2127d2bbf475fb3b25ff731f6e2ea2
LinkedFluuuush <linkedfluuuush@gmail.com>
2014-11-14 10:19:09 +0100

    Added package core

--- src/Core/Guerrier.java
+++ src/Core/Guerrier.java
@@ -1,11 +1,12 @@
+package Core;
 
-public class Voleur extends Personnage {
+public class Guerrier extends Personnage {
 
 	/**
 	 * @param nom
 	 * @param age
 	 */
-	public Voleur(String nom, int age) {
+	public Guerrier(String nom, int age) {
 		super(nom, age);
 		// TODO Auto-generated constructor stub
 	}
