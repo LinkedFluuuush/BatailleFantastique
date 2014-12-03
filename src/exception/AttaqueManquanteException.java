b3dc05eaaed0bf58539bd711689911b75d4883be
LinkedFluuuush <linkedfluuuush@gmail.com>
2014-12-03 14:34:56 +0100

    Corrections d'exceptions

--- src/exception/AttaqueManquanteException.java
+++ src/exception/AttaqueManquanteException.java
@@ -7,4 +7,22 @@ public class AttaqueManquanteException extends Exception {
 	 */
 	private static final long serialVersionUID = 929163054229792837L;
 
+    public AttaqueManquanteException() {
+    }
+
+    public AttaqueManquanteException(String message) {
+        super(message);
+    }
+
+    public AttaqueManquanteException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AttaqueManquanteException(Throwable cause) {
+        super(cause);
+    }
+
+    public AttaqueManquanteException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
--- src/exception/DeplacementException.java
+++ src/exception/DeplacementException.java
@@ -1,5 +1,22 @@
 package exception;
 
 public class DeplacementException extends Exception {
+    public DeplacementException() {
+    }
 
+    public DeplacementException(String message) {
+        super(message);
+    }
+
+    public DeplacementException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public DeplacementException(Throwable cause) {
+        super(cause);
+    }
+
+    public DeplacementException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
 }
